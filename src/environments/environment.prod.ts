72cb9d4bb6774dc2ff7af968e4a08eaeae90d0a2
Daniyil <daniyil.kudryavtsev@studiovatore.com>
2024-03-11 14:57:23 +0100

    cambio

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   urlApi:
-    'https://kkfm4r9ife.execute-api.eu-west-1.amazonaws.com/tenerife-experience/api/',
+    'https://8f5s1d9902.execute-api.eu-west-1.amazonaws.com/tenerife-experience-staging/api/',
   maxPrenotazione: 3,
   serviceId: 1,
 };
